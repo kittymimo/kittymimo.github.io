ef3b1aaec413bf7160f3ee75c181d02d3dc57eca
kittymimo <69246461+kittymimo@users.noreply.github.com>
2020-11-05 15:02:48 -0500

    Add files via upload

---
+++ stimuli.js
@@ -0,0 +1,8 @@
+var stimuli = [
+    {
+        phrase: "sound/test2.mp3",
+        image1: "img/blond_fache.png",
+        image2: "img/brune_heureuse.png",
+        bonne_reponse: "img/blond_fache.png",
+    }
+];
