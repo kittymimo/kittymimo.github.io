c81c1bdf39b0b5b7452f5ba87d0b98a8a4954065
kittymimo <69246461+kittymimo@users.noreply.github.com>
2020-12-09 13:36:44 -0500

    Add files via upload

---
+++ stimuli_v2-test.js
@@ -0,0 +1,53 @@
+var stimuli_pratique_v2 = [
+	{
+        version: 2,
+		bloc: 0,
+        ordre_logique: 3,
+        ordre_tache: "1",
+        emotion: "neutre",
+        sexe: "M",
+        mot: "sound/pensif.m4a",
+        image1: "img/pensif.jpeg",
+        image2: "img/heureux-gris.jpeg",
+        image3: "img/furieux-gris.jpeg",
+        bonne_reponse: 37
+    },
+];
+
+var stimuli_bloc1_v2 = [
+    {
+        version: 2,
+		bloc: 1,
+        ordre_logique: 5,
+        ordre_tache: "a1",
+        complexite: "simple",
+        position_emotion: "es",
+        emotion: "furieux",
+        sexe_sujet: "M",
+        phrase: "sound/simple-es-5.m4a",
+        image1: "img/Diapositive86.jpeg",
+        image2: "img/Diapositive87.jpeg",
+        image3: "img/Diapositive85.jpeg",
+        bonne_reponse: 39,
+        bonne_image: "img/Diapositive85.jpeg", 
+    },
+];
+
+var stimuli_bloc2_v2 = [
+    {
+        version: 2,
+		bloc: 2,
+        ordre_logique: 22,
+        ordre_tache: "a1",
+        complexite: "complexe",
+        position_emotion: "eo",
+        emotion: "neutre",
+        sexe_sujet: "F",
+        phrase: "sound/complexe-eo-4.m4a",
+        image1: "img/Diapositive64.jpeg",
+        image2: "img/Diapositive65.jpeg",
+        image3: "img/Diapositive66.jpeg",
+        bonne_reponse: 37,
+        bonne_image: "img/Diapositive64.jpeg", 
+    },
+];
